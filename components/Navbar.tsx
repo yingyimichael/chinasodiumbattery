914efc2d406b6dbd36dc3a2fa599cb43cd3cd4f3
yingyimichael <yingyimichael@gmail.com>
2026-04-17 22:56:57 +0800

    Create Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>My Site</h1>
+            {/* Add your navigation links here */}
+        </nav>
+    );
+};
+
+export default Navbar;

4b82d4a1d853f6e02228bce5aeb4c762468a2bda
yingyimichael <yingyimichael@gmail.com>
2026-04-17 23:06:00 +0800

    Add Footer component with copyright information

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-gray-400 p-6 mt-12">
+      <div className="max-w-7xl mx-auto text-center text-sm">
+        © 2026 China Sodium Battery. All rights reserved.
+      </div>
+    </footer>
+  );
+}

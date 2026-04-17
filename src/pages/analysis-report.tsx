5c8c72eb4c261bec38a895abc274cd120391bdd9
yingyimichael <yingyimichael@gmail.com>
2026-04-17 18:47:53 +0800

    Create AnalysisReport component

---
+++ src/pages/analysis-report.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnalysisReport = () => {
+    return (
+        <div>
+            <h1>Analysis Report</h1>
+        </div>
+    );
+};
+
+export default AnalysisReport;
